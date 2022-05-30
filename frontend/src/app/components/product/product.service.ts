dfaf829ad7b82d9969492f00e9ac45bf19142b4b
welliston <welliston10@gmail.com>
2022-05-30 11:27:02 -0300

    componente atualizar produto 1 e 2 (logica para atualizar)

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -32,7 +32,7 @@ export class ProductService {
   read(): Observable<Product[]>{
     return this.http.get<Product[]>(this.baseUrl)
   }
-
+ 
   readById(id: string): Observable<Product>{
     const url = `${this.baseUrl}/${id}`;
     return this.http.get<Product>(url);
